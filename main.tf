3ed07c8c325c0e325f0953b5245ce3132a9b5fde
eswar-sudo <eswarkumar501@gmail.com>
2025-09-08 11:12:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -35,19 +35,29 @@ provider "kubernetes" {
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
     command     = "aws"
-    args        = ["eks", "get-token", "--cluster-name", var.cluster_name, "--region", var.region]
+    args = [
+      "eks",
+      "get-token",
+      "--region", var.region,
+      "--cluster-name", var.cluster_name
+    ]
   }
 }
 
 provider "helm" {
-  kubernetes  {
+  kubernetes {
     host                   = data.aws_eks_cluster.this.endpoint
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
 
     exec {
       api_version = "client.authentication.k8s.io/v1beta1"
       command     = "aws"
-      args        = ["eks", "get-token", "--cluster-name", var.cluster_name, "--region", var.region]
+      args = [
+        "eks",
+        "get-token",
+        "--region", var.region,
+        "--cluster-name", var.cluster_name
+      ]
     }
   }
 }
