d55e6da95bfd7f42d2737cdc684231de327ec6fd
eswar-sudo <eswarkumar501@gmail.com>
2025-09-08 10:55:53 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -9,20 +9,26 @@ variable "cluster_name" {
   type        = string
 }
 
+# ---------------- Variables ----------------
+variable "oidc_provider_url" {
+  description = "EKS OIDC provider URL (without https://). Example: oidc.eks.us-east-1.amazonaws.com/id/XXXXXX"
+  type        = string
+}
+
 variable "namespace" {
-  description = "Kubernetes namespace where ExternalDNS is deployed"
+  description = "Namespace where external-dns will run"
   type        = string
   default     = "kube-system"
 }
 
-variable "external_dns_chart_version" {
-  description = "Helm chart version for ExternalDNS"
+variable "external_dns_irsa_role_name" {
+  description = "Name for the IAM role for ExternalDNS"
   type        = string
-  default     = "1.15.0"
+  default     = "external-dns-irsa-role"
 }
 
-variable "external_dns_irsa_role_name" {
-  description = "IAM Role name for ExternalDNS IRSA"
+variable "external_dns_chart_version" {
+  description = "ExternalDNS Helm chart version"
   type        = string
-  default     = "external-dns-irsa"
+  default     = "1.14.4"
 }
